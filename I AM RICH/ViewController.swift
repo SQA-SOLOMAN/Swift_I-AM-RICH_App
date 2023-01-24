c0c94f8f58e9287c3be85f7236d82cc295de46f2
SQA-SOLOMAN <kavkazus@hotmail.com>
2023-01-24 10:31:16 -0500

    Initial Commit

---
+++ I AM RICH/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I AM RICH
+//
+//  Created by Mike Soloman on 1/24/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
